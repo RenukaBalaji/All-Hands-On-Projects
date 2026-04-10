e3e3f92515a1f473b43671088ca91051016404a5
Renuka <renuka.balaji22@gmail.com>
2026-04-10 12:34:50 +0530

    Commit setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "Book_Recommender_System",
+    version = "0.1.0",
+    packages = find_packages(),
+    author="Renuka Balaji"
+    install_requires=[]
+}
