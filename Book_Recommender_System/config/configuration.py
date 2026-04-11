5711752cf4fdf6e2b1238e572cc163b9ba787321
Renuka <renuka.balaji22@gmail.com>
2026-04-11 15:02:13 +0530

    Data Validation Completed

--- Book_Recommender_System/config/configuration.py
+++ Book_Recommender_System/config/configuration.py
@@ -3,7 +3,7 @@ import sys
 from Book_Recommender_System.logger.log import logging
 from Book_Recommender_System.exception.exception_handler import AppException
 from Book_Recommender_System.utils.utils import read_yaml_file
-from Book_Recommender_System.entity.config_entity import DataIngestionConfig
+from Book_Recommender_System.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 import Book_Recommender_System.constant as constant_module
 CONFIG_FILE_PATH = constant_module.CONFIG_FILE_PATH
 
@@ -29,5 +29,32 @@ class AppConfiguration:
             logging.info(f"Data Ingestion Config: {response}")
             return response
         
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+    def get_data_validation_config(self) -> DataValidationConfig:
+        try:
+            data_validation_config = self.configs_info['data_validation_config']
+            data_ingestion_config = self.configs_info['data_ingestion_config']
+            dataset_dir = data_ingestion_config['dataset_dir']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
+            books_csv_file = data_validation_config['books_csv_file']
+            ratings_csv_file = data_validation_config['ratings_csv_file']
+
+            books_csv_file_dir = os.path.join(artifacts_dir, dataset_dir, data_ingestion_config['ingested_dir'], books_csv_file)
+            ratings_csv_file_dir = os.path.join(artifacts_dir, dataset_dir, data_ingestion_config['ingested_dir'], ratings_csv_file)
+            clean_data_path = os.path.join(artifacts_dir, dataset_dir, data_validation_config['clean_data_dir'])
+            serialized_objects_dir = os.path.join(artifacts_dir, data_validation_config['serialized_objects_dir'])
+
+            response = DataValidationConfig(
+                clean_data_dir = clean_data_path,
+                books_csv_file = books_csv_file_dir,
+                ratings_csv_file = ratings_csv_file_dir,
+                serialized_objects_dir = serialized_objects_dir
+            )
+
+            logging.info(f"Data Validation Config: {response}")
+            return response
+
         except Exception as e:
             raise AppException(e, sys) from e
--- Book_Recommender_System/entity/config_entity.py
+++ Book_Recommender_System/entity/config_entity.py
@@ -5,10 +5,10 @@ DataIngestionConfig = namedtuple("DatasetConfig", ["dataset_download_url",
                                                    "ingested_dir"])
 
 
-#DataValidationConfig = namedtuple("DataValidationConfig", ["clean_data_dir",
-#                                                         "books_csv_file",
-#                                                         "ratings_csv_file",
-#                                                         "serialized_objects_dir"])     
+DataValidationConfig = namedtuple("DataValidationConfig", ["clean_data_dir",
+                                                           "books_csv_file",
+                                                           "ratings_csv_file",
+                                                           "serialized_objects_dir"])     
 
 
 #DataTransformationConfig = namedtuple("DataTransformationConfig", ["clean_data_file_path",
