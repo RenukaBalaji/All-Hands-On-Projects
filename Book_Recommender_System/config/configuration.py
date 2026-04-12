a27e365e5ded556e593a001806b14663b96bb362
Renuka <renuka.balaji22@gmail.com>
2026-04-12 15:34:56 +0530

    Model Recommendation UI Added

--- Book_Recommender_System/config/configuration.py
+++ Book_Recommender_System/config/configuration.py
@@ -3,7 +3,7 @@ import sys
 from Book_Recommender_System.logger.log import logging
 from Book_Recommender_System.exception.exception_handler import AppException
 from Book_Recommender_System.utils.utils import read_yaml_file
-from Book_Recommender_System.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig)
+from Book_Recommender_System.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelRecommendationConfig)
 import Book_Recommender_System.constant as constant_module
 CONFIG_FILE_PATH = constant_module.CONFIG_FILE_PATH
 
@@ -103,6 +103,37 @@ class AppConfiguration:
             )
 
             logging.info(f"Model Trainer Config: {response}")
+            return response
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+    
+    def get_recommendation_config(self) -> ModelRecommendationConfig:
+        try:
+            recommendation_config = self.configs_info['recommendation_config']
+            model_trainer_config = self.configs_info['model_trainer_config']
+            data_validation_config = self.configs_info['data_validation_config']
+            trained_model_name = model_trainer_config['trained_model_name']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
+            trained_model_dir = os.path.join(artifacts_dir, model_trainer_config['trained_model_dir'])
+            poster_api = recommendation_config['poster_api_url']
+            
+
+            book_name_serialized_objects = os.path.join(artifacts_dir, data_validation_config['serialized_objects_dir'], 'book_names.pkl')
+            book_pivot_serialized_objects = os.path.join(artifacts_dir, data_validation_config['serialized_objects_dir'], 'book_pivot.pkl')
+            final_rating_serialized_objects = os.path.join(artifacts_dir, data_validation_config['serialized_objects_dir'], 'final_rating.pkl')
+
+            trained_model_path = os.path.join(trained_model_dir,trained_model_name)
+          
+            response = ModelRecommendationConfig(
+                book_name_serialized_objects = book_name_serialized_objects,
+                book_pivot_serialized_objects = book_pivot_serialized_objects,
+                final_rating_serialized_objects = final_rating_serialized_objects,
+                trained_model_path = trained_model_path
+            )
+
+            logging.info(f"Model Recommendation Config: {response}")
             return response
 
         except Exception as e:
